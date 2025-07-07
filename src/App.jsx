487e613c410e0168971a64d797a7b2465ddd6835
Sang Nguyen <sorayaku42@gmail.com>
2025-07-07 22:56:00 +0700

    fewfew

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,37 @@ import { useState } from "react";
 
 function App() {
 
-  const [isOpen, setIsOpen] = useState(false);
 
-const toggleSidebar = () => {
-  setIsOpen(!isOpen);
-};
   return (
-    <div class="min-h-80"> 
-      {
-        isOpen ? <div className={`static top-0 bg-gray-100 h-48 w-[120px] duration-200 ease-in ${isOpen ? 'left-0' : '-left-[200px]'}`}>
-         <p onClick={toggleSidebar} class="ml-4"> close </p>
-    </div> : 
-           <button onClick={toggleSidebar} class="text-black pl-2 pt-2">☰</button>
-      }
-      <div class="flex items-center justify-end pr-2 text-base sm:text-sm xl:text-base">
-        <a class="mr-2 cursor-pointer" target="_blacnk" rel="noopener noreferrer" href="http://gmail.com/"> gmail </a>
-         <a class="cursor-pointer" target="_blacnk" rel="noopener noreferrer" href="http://gmail.com/"> images </a>
-        <img src="/info.png" class="w-4 mx-3"/>
-        <span class="rounded-full bg-pink-400 w-6 h-6 mt-1 xl:mr-10 sm:mr-2"> </span>
-      </div>
- <div class="flex flex-col items-center">
-      <img class="sm:w-[140px] sm:h-[140px] size-16 object-cover mb-5 mt-20 sm:w-30 xl:w-24 xl:h-24" src="/google.webp"/>
-      <div class="border border-solid border-gray-500 rounded-3xl flex items-center w-5/6 py-2 shrink sm:w-5/6 xl:w-3/5 xl:py-2 shadow-md">
-        <img src="/search.png" class="w-3 h-3 mr-4 ml-2 object-cover"/>
-        <p class="text-gray-400 text-[12px] max-w-96 w-72"> Tìm kiếm google hoặc nhập một url</p>
-        <div class="flex flex-row items-cente justify-end flex-1 pr-2">
-          <img src="/voice.png" class="w-3 ml-15 mr-3"/>
-           <img src="/camera.jpg" class="w-5 mr-5"/>
-        </div>
-      </div>
-
-
+  <div className="bg-gradient-to-t from-violet-200 to-fuchsia-500 h-screen xl:text-xl">
+    <div className="flex justify-around items-center">
+     <img src="/Sang.png" className="object-cover h-20 w-20"/>
+     <div className="flex items-center">
+     <a className="text-sky-500 mr-2"> Contact </a>
+     <a className="text-sky-500"> About me </a>
+     </div>
     </div>
 
-   
+    <div className="flex flex-col items-center mt-40 justify-center text-center xl:mt-50">
+  <h1 className="text-2xl text-black-400"> I'm Sang</h1>
+  <p className="mt-2">A web developer or freelancer that make your site go to online and public </p>
+     <div className="flex gap-2 mt-4">
+     <a className="px-5 py-2 bg-green-500 text-white inline"> Contact </a>
+     <a className="px-5 py-2 bg-green-500 text-white inline"> Info </a>
+     </div>
 
+         <div className="text-white text-left mt-20 italic xl:text-lg">
+      <p> email: sorayku42@gmail.com </p>
+      <p> phone: +04 0907035042 </p>
     </div>
+    </div>
+
+  </div>
 
   );
 }
 
 export default App;
+
+
+
